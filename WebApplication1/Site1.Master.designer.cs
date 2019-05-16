2192235398aebc757d4458e4b96a45ffdc41d1ec
liam34 <48293076+liam34@users.noreply.github.com>
2019-05-16 16:48:45 -0600

    site1 master modifies

--- WebApplication1/Site1.Master.designer.cs
+++ WebApplication1/Site1.Master.designer.cs
@@ -22,13 +22,13 @@ namespace WebApplication1 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// BulletedList2 control.
+        /// BulletedList1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.BulletedList BulletedList2;
+        protected global::System.Web.UI.WebControls.BulletedList BulletedList1;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
