20877edea0aafe744cfe100687768266bd94d4f6
Drew <carldrewlamb@gmail.com>
2019-05-21 11:44:57 -0600

    Reg page done

--- WebApplication1/Registration_page.aspx
+++ WebApplication1/Registration_page.aspx
@@ -1,5 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Registration_page.aspx.cs" Inherits="WebApplication1.Registration_page" %>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    content here
+    <form action="/" method="post">
+        First Name: <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+        Last Name: <br />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+        Phone: <br />
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br />
+        City: <br />
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox><br /><br />
+        <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+    </form>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
 </asp:Content>
--- WebApplication1/Registration_page.aspx.cs
+++ WebApplication1/Registration_page.aspx.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace WebApplication1
 {
     public partial class Registration_page : System.Web.UI.Page
     {
+        SqlConnection conn = new SqlConnection(@"Data Source=softdev;Initial Catalog=Marina;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //start code here
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "INSERT INTO Customer VALUES('"+TextBox1.Text+"','"+TextBox2.Text+"','"+TextBox3.Text+"','"+TextBox4.Text+"')";
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
         }
     }
 }
